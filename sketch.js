dc406b2630c124a7d79db5a8678867264c4010cf
AaravVikram <66343312+AaravVikram@users.noreply.github.com>
2020-08-02 23:34:19 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var ball, ground, box1, box2, box3;
+var trashImg, sling;
+
+function preload()
+{
+	trashImg = loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = new Paper(100,600,20);
+
+	Engine.run(engine);
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	
+	box1=createSprite(1050, 530, 200,20);
+	
+	box1.addImage(trashImg);
+
+	sling = new Launcher(ball.body,{x:500,y:400})
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  Engine.update(engine);
+  ball.display();
+  drawSprites();
+  rect(ground.position.x, ground.position.y,width,10);
+ 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:75,y:-100})
+	}
+	
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(ball.body, {x: mouseX, y: mouseY})
+}
+
+function mouseReleased(){
+    sling.fly();
+}
+
+
